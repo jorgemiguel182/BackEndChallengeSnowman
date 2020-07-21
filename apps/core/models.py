7cb831aba65b2a0063f72a11c4f6f7fd1f636219
jmdinfo123@gmail.com <jmdinfo123@gmail.com>
2020-07-21 17:11:21 -0300

    criando bd

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class TbPicture(models.Model):
+    id = models.AutoField(primary_key=True)
+    picture = models.ImageField()
+    tourist_spot = models.ForeignKey('TbTouristSpot', models.DO_NOTHING)
+
+
+class TbCategory(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.name
+
+
+class TbUserFavorite(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, models.DO_NOTHING)
+    tourist_spot = models.ForeignKey('TbTouristSpot', models.DO_NOTHING)
+    
+    
+class TbTouristSpot(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=400)
+    picture = models.ForeignKey(TbPicture, models.DO_NOTHING)
+    geo_location = models.CharField(max_length=100)
+    category = models.ForeignKey(TbCategory, models.DO_NOTHING)
+    
+    def __str__(self):
+        return self.name
