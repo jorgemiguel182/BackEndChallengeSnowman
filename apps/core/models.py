fef4e47d6f63ee3bf7ebab03730daea2c15288f6
jmdinfo123@gmail.com <jmdinfo123@gmail.com>
2020-07-21 22:22:36 -0300

    ajuste em nome de tabelas

--- apps/core/models.py
+++ apps/core/models.py
@@ -6,20 +6,29 @@ class TbPicture(models.Model):
     id = models.AutoField(primary_key=True)
     picture = models.ImageField()
     tourist_spot = models.ForeignKey('TbTouristSpot', models.DO_NOTHING)
+    
+    class Meta:
+        db_table = 'TB_PICTURE'
 
 
 class TbCategory(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=400)
-
+    
+    class Meta:
+        db_table = 'TB_CATEGORY'
+        
     def __str__(self):
         return self.name
-
+    
 
 class TbUserFavorite(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, models.DO_NOTHING)
     tourist_spot = models.ForeignKey('TbTouristSpot', models.DO_NOTHING)
+    
+    class Meta:
+        db_table = 'TB_USER_FAVORITE'
     
     
 class TbTouristSpot(models.Model):
@@ -29,5 +38,8 @@ class TbTouristSpot(models.Model):
     geo_location = models.CharField(max_length=100)
     category = models.ForeignKey(TbCategory, models.DO_NOTHING)
     
+    class Meta:
+        db_table = 'TB_TOURIST_SPOT'
+        
     def __str__(self):
         return self.name
