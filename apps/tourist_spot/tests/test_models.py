2b29a96548d0abf2fd5c7477ebb72f2507e792d9
jmdinfo123@gmail.com <jmdinfo123@gmail.com>
2020-07-26 23:06:25 -0300

    refactor test_models, comentado teste_api para fazer depois, pois sera necessário nova configuração após a ativação do auth token

--- apps/tourist_spot/tests/test_models.py
+++ apps/tourist_spot/tests/test_models.py
@@ -1,15 +1,25 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-from apps.core.models import TbTouristSpot, TbCategory
+from apps.core.models import TbTouristSpot, TbCategory, TbUserFavorite
 
 
-class TouristPointModelTest(TestCase):
-    """Test module for TouristPoint model"""
+class TouristPointModelsTest(TestCase):
+    """Test module for TouristPoint model and correlations"""
     def setUp(self):
+        user = User.objects.create(password='test@123', username='teste_api', email='teste@teste.com')
         category = TbCategory.objects.create(name="Categoria 1")
-        category.save
-        TbTouristSpot.objects.create(name="Teste1", geo_location="123", category=category)
+        v_tourist_spot = TbTouristSpot.objects.create(name="Teste1", geo_location="123", category=category)
+        TbUserFavorite.objects.create(tourist_spot=v_tourist_spot, user=user)
 
     def test_category_single(self):
+        caregory = TbCategory.objects.filter().first()
+        self.assertEquals(caregory.name, 'Categoria 1')
+
+    def test_tourist_spot_single(self):
         tourist_spot = TbTouristSpot.objects.filter().first()
         self.assertEquals(tourist_spot.name, 'Teste1')
+
+    def test_user_favorite_single(self):
+        user_favorite = TbUserFavorite.objects.filter().first()
+        self.assertEquals(user_favorite.user.username, 'teste_api')
