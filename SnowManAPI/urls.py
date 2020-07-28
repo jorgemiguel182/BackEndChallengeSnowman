d312abe809932ded14bcf46196bacaff1544436a
jmdinfo123@gmail.com <jmdinfo123@gmail.com>
2020-07-28 18:15:14 -0300

    ajuste settings heroku statics

--- SnowManAPI/urls.py
+++ SnowManAPI/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -48,3 +49,4 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
